183423da881488b0fb4e21901d6bd723e0abcde1
Pranav <pranavharnaiya@gmail.com>
2026-02-03 11:31:04 +0530

    read

--- node js/index.js
+++ node js/index.js
@@ -1,20 +1,10 @@
-import http from 'http';
+const fs=require('fs');
 
-const server = http.createServer((req, res) => {
-   if(req.url === '/'){
-      res.end('welcome to our home page');
-   }
-   else if(req.url === '/this_is_content_page'){
-    res.end('<h1>We are Student of ABES ENGG COLLEGE</h1><img src="https://avatars.githubusercontent.com/u/48626910?s=280&v=4" alt="College Image">');
-   }
-    else if(req.url === '/contact'){
-    res.end('Contact us at: 123-456-7890');
-   }
-   else{
-    res.end('404 page is not found');
+// fs.writeFileSync('./ds-b','Hello');
+// fs.writeFileSync('./abes.txt','Hello World!');
 
-   }
+const result=fs.readFileSync('./abes.txt','utf-8');
+console.log(result);
 
-});
-
-server.listen(8080,()=>console.log('Server is running on port 8080'));
+const result2=fs.readFileSync('./ds-b','utf-8');
+console.log(result2);
